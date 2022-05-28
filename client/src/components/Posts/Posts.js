486737c954f272714ac63302475d1b8c5bcaff1c
theDTS1 <orel1one@gmail.com>
2022-05-29 01:10:06 +0300

    changes

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//import post component
+import Post from "./POST/Post";
+
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+export default Posts;
