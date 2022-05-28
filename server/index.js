080bfbe482ee91a851c10c5f131fa80fe640a24e
theDTS1 <orel1one@gmail.com>
2022-05-28 20:07:20 +0300

    change to index.js

--- server/index.js
+++ server/index.js
@@ -2,3 +2,13 @@ import { Express } from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = Express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+// connect to MongoDB
