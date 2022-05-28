544c20a7b5016eaecf7c689b1249a58abe04f73d
theDTS1 <orel1one@gmail.com>
2022-05-28 20:36:40 +0300

    connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-const app = Express();
+// Import dotenv in ES6!
+import dotenv from "dotenv";
+dotenv.config({ path: "dev.env" });
+
+const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
@@ -12,3 +16,14 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // connect to MongoDB
+
+const mongoConnect = process.env.MONGODBKEY;
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(mongoConnect, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
