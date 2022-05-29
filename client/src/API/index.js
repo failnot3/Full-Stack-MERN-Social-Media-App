0305dbd968f46a3f7d5fc26a67689e9774b99e8c
theDTS1 <orel1one@gmail.com>
2022-05-30 00:29:47 +0300

    client/API changes

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -5,8 +5,7 @@ const url = "http://localhost:5000/posts";
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+// updatePost - first parameter is ID, second a.k.a. updatePost is the post itself.
 export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
-// updatePost - first parameter is ID, second a.k.a. updatePost is the post itself.
-
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
