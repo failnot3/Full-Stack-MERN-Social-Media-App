da1d1703ef5980a5da2207a22d068756cc4da7ac
theDTS1 <orel1one@gmail.com>
2022-05-30 00:58:16 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -10,13 +10,10 @@ import postRoutes from "./routes/posts.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "dev.env" });
 
-const app = express();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors());
+
 app.use("/posts", postRoutes);
 
 // connect to MongoDB
@@ -28,6 +25,8 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(mongoConnect, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(`${error} did not connect`));
